7e01ab4d201e700fc7d55af465dc39632ec3d36c
«Tanya» <itsmetanya@mail.ru>
2023-04-05 14:29:12 +0300

    задача1

--- task1/Program.cs
+++ task1/Program.cs
@@ -10,15 +10,15 @@ int number = int.Parse(Console.ReadLine());
 
 int num1 = number / 10000 % 10;
 int num2 = number / 1000 % 10;
-int num3 = number / 10 % 10;
-int num4 = number % 10;
-int num5 = number / 10000;
+int num4 = number / 10 % 10;
+int num5 = number % 10;
+int num6 = number / 10000;
         
-if (num5 < 1 || num5 > 9)
+if (num6 < 1 || num6 > 9)
 {
 Console.WriteLine(number + " -> " + "Not 5-digit number");
 }
-else if  (num1 == num4 && num2 == num3)
+else if  (num1 == num5 && num2 == num4)
 {
 Console.WriteLine(number + " -> Yes, this is palindrom");
 }
